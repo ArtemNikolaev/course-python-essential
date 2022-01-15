6234b3474468a546e41864cf2eca4aa198d76773
Artem Nikolaev <artem@nikolaev.by>
2022-01-15 17:39:29 +0300

    4.1 - расчет зп #22

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Added
+- [4.1](https://github.com/ArtemNikolaev/gb-hw/issues/22) - расчет зп
 - заглушки для задач 4-ой домашки
 - ссылка на задачу в файле с решением
 
--- hw4/4.1.py
+++ hw4/4.1.py
@@ -1,1 +1,31 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/22
+
+from sys import argv, exit
+from shared.number import can_be_float
+
+
+def validate_arguments():
+    if len(argv) < 4:
+        print('Скрипт нужно запускать с тремя параметрами:')
+        print('\t- отработанные часы')
+        print('\t- часовая ставка')
+        print('\t- премия')
+        exit()
+
+
+def to_number(value):
+    if can_be_float(value):
+        return float(value)
+    else:
+        print('Все параметры должны быть числовыми')
+        exit()
+
+
+def calculate_salary():
+    validate_arguments()
+    hours, rate, bonus = (to_number(argv[i]) for i in range(1, 4))
+
+    return hours * rate + bonus
+
+
+print(calculate_salary())
