7ea45766e8c157e5a65cde69ecce4967b3d9f069
Artem Nikolaev <artem@nikolaev.by>
2022-01-16 01:43:18 +0300

    4.6    print('Result for int generator')  #27

--- hw4/4.6.py
+++ hw4/4.6.py
@@ -1,22 +1,47 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/27
 from shared.number import is_not_int
 from sys import exit
+from itertools import cycle, count
 
 
-def int_generator(value):
-    if is_not_int(value):
+# итератор, генерирующий целые числа, начиная с указанного;
+def int_generator(from_value, to_value):
+    if is_not_int(from_value):
         print('Cтартовое значение должно быть целым')
         exit()
-    while True:
-        yield value
-        value += 1
+    while from_value <= to_value:
+        yield from_value
+        from_value += 1
 
 
 def test_int_generator():
-    for i in int_generator(100):
-        if i > 1000:
-            exit()
-        print(i)
+    print('Result for int generator')
+    for i in int_generator(from_value=100, to_value=105):
+        print('\t', i)
 
 
+# итератор, повторяющий элементы некоторого списка, определённого заранее.
+def list_repeater(array, index_from, index_to, repeat_times):
+    counter = count(1, 1)
+    for i in cycle(array[index_from:index_to+1]):
+        if next(counter) == repeat_times:
+            return i
+        else:
+            yield i
+
+
+def test_list_repeater():
+    print('Result for int generator')
+    test_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    for i in list_repeater(
+        array=test_list,
+        index_from=1,
+        index_to=7,
+        repeat_times=15
+    ):
+        print('\t', i)
+
+
+# run tests
 test_int_generator()
+test_list_repeater()
