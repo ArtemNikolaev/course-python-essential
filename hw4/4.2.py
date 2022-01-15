845f7699d270811cad97eb6650decc7affc98a1c
Artem Nikolaev <artem@nikolaev.by>
2022-01-15 18:24:53 +0300

    4.2 - выборка из массива чисел, которые больше предыдущего элемента #23

--- hw4/4.2.py
+++ hw4/4.2.py
@@ -1,1 +1,12 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/23
+def run(array):
+    return (
+        array[i]
+        for i in range(1, len(array))
+        if array[i] > array[i-1]
+        )
+
+
+test_input = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+print(list(run(test_input)))
