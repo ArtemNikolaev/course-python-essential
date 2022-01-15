1fc9577eacb7ad707683f3c3f693e26464128dbc
Artem Nikolaev <artem@nikolaev.by>
2022-01-15 18:39:48 +0300

    4.5 - произведение четных от 100 до 1001 #26

--- hw4/4.2.py
+++ hw4/4.2.py
@@ -4,7 +4,7 @@ def run(array):
         array[i]
         for i in range(1, len(array))
         if array[i] > array[i-1]
-        )
+    )
 
 
 test_input = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
