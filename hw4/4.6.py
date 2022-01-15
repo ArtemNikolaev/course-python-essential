46718b271e3d0860722c30c050cd4e14c1768a32
Artem Nikolaev <artem@nikolaev.by>
2022-01-15 19:11:19 +0300

    4.6.1 - in progress

--- hw4/4.6.py
+++ hw4/4.6.py
@@ -1,1 +1,22 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/27
+from shared.number import is_not_int
+from sys import exit
+
+
+def int_generator(value):
+    if is_not_int(value):
+        print('Cтартовое значение должно быть целым')
+        exit()
+    while True:
+        yield value
+        value += 1
+
+
+def test_int_generator():
+    for i in int_generator(100):
+        if i > 1000:
+            exit()
+        print(i)
+
+
+test_int_generator()
