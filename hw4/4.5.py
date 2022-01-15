3a62e19eba1c10af997a09eedf4e67771876f2fa
Artem Nikolaev <artem@nikolaev.by>
2022-01-15 18:39:48 +0300

    4.5 - произведение четных от 100 до 1001 #26

--- hw4/4.5.py
+++ hw4/4.5.py
@@ -1,1 +1,13 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/26
+from functools import reduce
+
+
+def even_100_1000():
+    return (i for i in range(100, 1001) if i % 2 == 0)
+
+
+def multiply():
+    return reduce(lambda a, b: a * b, even_100_1000())
+
+
+print(multiply())
