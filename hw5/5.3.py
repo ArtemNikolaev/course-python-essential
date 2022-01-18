b39e0ae7aefc250282de9897c09430e72fbce6d5
Artem Nikolaev <artem@nikolaev.by>
2022-01-18 10:01:02 +0300

    5.4 - заменить английские числительные на русские #34

--- hw5/5.3.py
+++ hw5/5.3.py
@@ -2,24 +2,22 @@
 import io
 from itertools import count
 
-def run():
-    f = io.open('5.3.txt', 'r', encoding='utf-8')
-    salarySum = 0
-    peoples = 0
+f = io.open('5.3.txt', 'r', encoding='utf-8')
+salarySum = 0
+peoples = 0
 
+string = f.readline()
+
+while len(string) > 0:
+    peoples += 1
+
+    name, salaryStr = string.split()
+    # todo: after second dz merged check input with can_be_float
+    salary = float(salaryStr)
+    salarySum += salary
+    if salary < 20000:
+        print(string)
     string = f.readline()
-    while len(string) > 0:
-        peoples += 1
-
-        name, salaryStr = string.split()
-        # todo: after second dz merged check input with can_be_float
-        salary = float(salaryStr)
-        salarySum += salary
-        if salary < 20000:
-            print(string)
-        string = f.readline()
-    print('\nCредняя зарплата: ', salarySum / peoples)
 
 
-
-run()
+print('\nCредняя зарплата: ', salarySum / peoples)
