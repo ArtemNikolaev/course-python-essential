e71fcf73140f670f2853b92e61fd3698f86a1b3e
Artem Nikolaev <artem@nikolaev.by>
2022-01-18 09:25:18 +0300

    5.2 - подсчёт строк и слов в каждой строке #32

--- hw5/5.1.py
+++ hw5/5.1.py
@@ -1,10 +1,13 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/30
 
-f = open('5.1.txt', 'w')
-str = 'not empty string'
+def run():
+    f = open('5.1.txt', 'w')
+    str = 'not empty string'
 
-while(len(str) > 0):
-    str = input('Введите строку для записи или нажмите Enter для завершения: ')
-    f.write(str + '\n')
+    while(len(str) > 0):
+        str = input('Введите строку для записи или нажмите Enter для завершения: ')
+        f.write(str + '\n')
 
-f.close()
+    f.close()
+
+run()
