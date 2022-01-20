91102ffcb86d260b7bb3434e0013999e4a3e1567
Artem Nikolaev <artem@nikolaev.by>
2022-01-20 08:53:53 +0300

    5.7 - file to json with calculations #37

--- hw5/5.7.py
+++ hw5/5.7.py
@@ -1,1 +1,45 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/37
+import io
+from json import dumps
+
+
+def read_file(filename):
+    f = io.open(filename, 'r', encoding='utf-8')
+    string = f.readline()
+    while string:
+        yield string
+        string = f.readline()
+
+
+def calculate_result(strings):
+    average_sum = 0
+    average_count = 0
+    firm_dict = {}
+
+    for string in strings:
+        name, trash, income, outcome = string.split()
+        profit = float(income) - float(outcome)
+        firm_dict[name] = profit
+        if profit > 0:
+            average_sum += profit
+            average_count += 1
+    return [
+        firm_dict,
+        {"average_profit": average_sum / average_count}
+    ]
+
+
+def save_result(filename, result):
+    with open(filename, 'w') as opened_file:
+        opened_file.write(
+            dumps(result)
+        )
+
+
+def run(file_from, file_to):
+    strings = read_file(file_from)
+    result = calculate_result(strings)
+    save_result(file_to, result)
+
+
+run(file_from='5.7.txt', file_to='5.7.result.txt')
