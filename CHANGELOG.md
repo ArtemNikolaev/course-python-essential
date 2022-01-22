5aea2a17c74dfb0a56aad757c5e89f039170e6d2
Artem Nikolaev <artem@nikolaev.by>
2022-01-22 22:01:11 +0300

    6.1 - светофор #38

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- [6.1](# https://github.com/ArtemNikolaev/gb-hw/issues/38
+) - светофор
 
 ## [HomeWork5] - 2021-01-26
 ### Added
--- hw6/6.1.py
+++ hw6/6.1.py
@@ -1,1 +1,24 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/38
+from itertools import cycle
+from time import sleep
+
+
+class TrafficLight:
+    __rules = {
+        '🔴': 7,
+        '🟡': 2,
+        '🟢': 4,
+    }
+    __lights = ('🔴', '🟡', '🟢')
+    __lights_cycle = cycle(__lights)
+    __color = next(__lights_cycle)
+
+    def running(self):
+        print(self.__color)
+        sleep(self.__rules[self.__color])
+        self.__color = next(self.__lights_cycle)
+        self.running()
+
+
+light1 = TrafficLight()
+light1.running()
