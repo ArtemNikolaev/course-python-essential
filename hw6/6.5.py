250915b60dadbd547192cf976ae0438cbbfe3237
Artem Nikolaev <artem@nikolaev.by>
2022-01-24 19:51:44 +0300

    6.5 - Stationery - #42

--- hw6/6.5.py
+++ hw6/6.5.py
@@ -1,1 +1,32 @@
 # https://github.com/ArtemNikolaev/gb-hw/issues/42
+class Stationery:
+    title = 'Канцелярская херня'
+
+    def draw(self):
+        print(self.title + ' чот рисует')
+
+
+class Pen:
+    title = 'Pen'
+
+    def draw(self):
+        print(self.title + ' penning')
+
+
+class Pencil:
+    title = 'Pencil'
+
+    def draw(self):
+        print(self.title + ' pencilling')
+
+
+class Handle:
+    title = 'Handle'
+
+    def draw(self):
+        print(self.title + ' handling')
+
+
+Pen().draw()
+Pencil().draw()
+Handle().draw()
